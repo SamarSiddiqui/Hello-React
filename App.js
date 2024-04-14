2304f0f02d8ce06d102a7c8bf7a3a6ac1943c65e
SamarSiddiqui <samarsiddiqui3010@gmail.com>
2024-04-14 14:59:19 +0530

    added type module

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
 let heading = React.createElement('h1',{},'Hello From React')
 
 let root = ReactDOM.createRoot(document.getElementById('root'))
--- dist/index.6bd02f5a.js
+++ dist/index.6bd02f5a.js
@@ -1,2 +1,6 @@
+impor;
+let heading = React.createElement("h1", {}, "Hello From React");
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
 
 //# sourceMappingURL=index.6bd02f5a.js.map
