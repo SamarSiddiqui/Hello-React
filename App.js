a3d834743e161e66be3085a78c9d70d40c06a7a1
SamarSiddiqui <samarsiddiqui3010@gmail.com>
2024-04-16 18:10:43 +0530

    Learned And Used  Props

--- App.js
+++ App.js
@@ -1,31 +1,90 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-let root = ReactDOM.createRoot(document.getElementById('root'))
+import imageSrc from "./logo.png"
+
+// Header 1) logo 2) About,Contact,Cart
+// Body  1) SearchBar 2) FoodContainer 2.1) ResCards
 
 
-//React Element
-let heading = React.createElement('h1',{},'Hello I am React Element.💕')
+let Header = ()=>{
+ return <div className="header">        
+      <div className="logo">
+      <img src= {imageSrc} alt="Logo" />
+        </div>      
+      <div className="options">
+      <ul>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+        </div>      
 
-let span = <span>Hola  </span>
+  </div>
+}
 
-//React Element Using Jsx
-let Title =  <h1>{span}Hello From Jsx.{span}😊</h1>
-// JsxCode ==> ReactElement ==> JsObject ==> Html element
+let Rescards = ({name,cuisines,locality})=>{
+  
+  return <div className="rescard">
+    <div className="resPhoto">
+           
+    </div>
+
+    <h4>{name}</h4>  
+    <h4>{cuisines}</h4>
+    <h4>{locality}</h4>
+  </div>
+}
 
 
-// ReactComponents  (Functional  Components)
+let resObj = {
+                    "info": {
+                      "id": "60379",
+                      "name": "Pizza Hut",
+                      "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+                      "locality": "Saharaganj Mall",
+                      "areaName": "Hazratganj",
+                      "costForTwo": "₹350 for two",
+                      "cuisines": [
+                        "Pizzas"
+                      ],
+                      "avgRating": 4.3,
+                      "parentId": "721",
+                      "avgRatingString": "4.3",
+                      "totalRatingsString": "1K+",
+                       "availability": {
+                        "nextCloseTime": "2024-04-16 23:00:00",
+                        "opened": true
+                      }
+                    }
+}
+let Body = ()=>{
+ return <div className="resContainer">
+    <div className="searchBar">
+      <h2>Search Bar</h2>
+    </div>
+    <div className="resArea">       
+       <Rescards name="Pizza Hut" cuisines="Pizza" locality="HazratGanj"/>
+       <Rescards name="Idrees Biryani" cuisines="Biryani" locality="Chowk"/>
+       <Rescards name="Al Baik" cuisines="Zinger Burger" locality="Gomti Nagar"/>
+       <Rescards name="Tunday" cuisines="Kebaab" locality="Aminabad"/>
+       
+       
+       
+       
+        
+    </div>
 
-let HeadingComponent = () =>{
-    return <div>
-        {heading}
-        {Title}
-        <h1>Hello from Functional Component.😍</h1>
+  </div>
+}
+let MainContainer = ()=>{
+ return <div className="main">
+      <Header />
+      <Body />
+      <h1>Hello💕</h1>
     </div>
 }
 
 
 
-//   < />
-root.render(<HeadingComponent />)
-// console.log(heading);
-// console.log(title);
+let root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<MainContainer />)
