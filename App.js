d15c0a55f5e072a23b26434d4e49be4880465f0c
SamarSiddiqui <samarsiddiqui3010@gmail.com>
2024-04-16 10:33:39 +0530

    Learned Jsx And Functional Component

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react"
+import ReactDOM from "react-dom/client"
+let root = ReactDOM.createRoot(document.getElementById('root'))
 
-let heading = React.createElement('h1',{},'Hello From React💕')
 
-const root = ReactDOM.createRoot( document.getElementById("root")); 
-// const oot = createRoot(rootElement);
-root.render(heading)
+//React Element
+let heading = React.createElement('h1',{},'Hello I am React Element.💕')
+
+let span = <span>Hola  </span>
+
+//React Element Using Jsx
+let Title =  <h1>{span}Hello From Jsx.{span}😊</h1>
+// JsxCode ==> ReactElement ==> JsObject ==> Html element
+
+
+// ReactComponents  (Functional  Components)
+
+let HeadingComponent = () =>{
+    return <div>
+        {heading}
+        {Title}
+        <h1>Hello from Functional Component.😍</h1>
+    </div>
+}
+
+
+
+//   < />
+root.render(<HeadingComponent />)
+// console.log(heading);
+// console.log(title);
