67077821607e0ecd84f0c6da0868d7d6c98dc0d2
SamarSiddiqui <samarsiddiqui3010@gmail.com>
2024-04-20 20:14:10 +0530

    Learned About Hooks (useSatate)

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,24 @@
 import Rescards from "./Rescards"; 
 import resList from "../utils/mock";
-
+import { useState } from "react";
 // console.log(resList);
 let Body = ()=>{
-   
+  let [ listOfRes,SetListOfRes] = useState(resList)
    
     return <div className="resContainer ">
        <div className="filter">
-        
+       <button className="filter-btn" onClick={()=>{
+         let filterRes= listOfRes.filter((res)=>
+            res.data.avgRating > 4.1
+          )
+          SetListOfRes(filterRes)
+          console.log(filterRes);
+        }}>Click To Filter Restro Above 4⭐</button>
+
        </div>
        <div className="resArea">
     {  
-      resList.map((restaurant,index)=>(
+      listOfRes.map((restaurant,index)=>(
        <Rescards key={index} resData={restaurant}/>
       
      ))
